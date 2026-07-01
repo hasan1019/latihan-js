eb99b52446b11e883b6d0a65ae3c07625954d97f
hasan1019 <hasanfariki@students.unnes.ac.id>
2026-07-02 01:02:02 +0700

    learn let & const to create variable

---
+++ app.js
@@ -0,0 +1,8 @@
+let massage = "hello gais!";
+let me = "my name is";
+let you = "who are you ?";
+
+let us = massage;
+
+massage = "ini aku bajak";
+alert(massage + " " + me + " " + you + "," + us);

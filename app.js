13e7c89f8e184ed7e174af1a4bbce2df8c7e73cb
hasan1019 <hasanfariki@students.unnes.ac.id>
2026-07-03 22:43:35 +0700

    switch case

--- app.js
+++ app.js
@@ -1,56 +1,16 @@
-// let hour = 12;
-// let isWeekend = false;
+let a = 6;
 
-// if (hour < 10 || hour > 18 || isWeekend) {
-//   alert( 'The office is closed.' ); // it is the weekend
-// } else {
-//     alert( 'The office is open.' ); // it is a weekday
-// }
+switch (a) {
+  case 4:
+    alert('Right!');
+    break;
 
-// let firstName = "";
-// let lastName = "";
-// let nickName = "SuperCoder";
+  case 3: // (*) grouped two cases
+  case 5:
+    alert('Wrong!');
+    alert("Why don't you take a math class?");
+    break;
 
-// alert( firstName || lastName || nickName || "Anonymous"); // SuperCoder
-
-// true && alert("not");
-// false || alert("yes");
-
-// let age = 80;
-
-// if (age >= 14 && age <= 90) {alert("you are in the range");} else {alert("you are not")};
-
-// let role = prompt("who's there?");
-
-// if (role === "Admin"){
-//     let password = prompt("enter pass!!!");
-    
-//     if (password === "TheMaster"){
-//         alert("you may enter");
-//     }else if(password === "Other"){
-//         alert("who are you???");
-//     }else{
-//         alert("it's a anonym");
-//     }
-// }else if(role === "" || role ===null){
-//     alert("canceled");
-// }else{
-//     alert("i don't know you");
-// }
-
-// let dompet = null;
-
-// alert (dompet ?? 100);
-
-outer : for (let i = 0; i < 3; i++) {
-
-  for (let j = 0; j < 7; j++) {
-
-    if(j%2 == 1) continue;
-    let input = prompt(`Value at coords (${i},${j})`);
-    if (!input) break outer;
-
-  }
+  default:
+    alert('The result is strange. Really.');
 }
-
-alert('Done!');

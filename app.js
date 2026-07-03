c0e7d6880194cee76509090fbc8447e4cc89e9b3
hasan1019 <hasanfariki@students.unnes.ac.id>
2026-07-03 21:27:54 +0700

    while for n loop, nullis operator ??

--- app.js
+++ app.js
@@ -1,13 +1,56 @@
-let massage;
-let age = prompt("enter your age :");
+// let hour = 12;
+// let isWeekend = false;
 
-if (age < 18){
-    massage = "you are not allowed";
-} else if (age > 18){
-    massage = "you are to old";
-}else{
-    massage = "you may in";
+// if (hour < 10 || hour > 18 || isWeekend) {
+//   alert( 'The office is closed.' ); // it is the weekend
+// } else {
+//     alert( 'The office is open.' ); // it is a weekday
+// }
+
+// let firstName = "";
+// let lastName = "";
+// let nickName = "SuperCoder";
+
+// alert( firstName || lastName || nickName || "Anonymous"); // SuperCoder
+
+// true && alert("not");
+// false || alert("yes");
+
+// let age = 80;
+
+// if (age >= 14 && age <= 90) {alert("you are in the range");} else {alert("you are not")};
+
+// let role = prompt("who's there?");
+
+// if (role === "Admin"){
+//     let password = prompt("enter pass!!!");
+    
+//     if (password === "TheMaster"){
+//         alert("you may enter");
+//     }else if(password === "Other"){
+//         alert("who are you???");
+//     }else{
+//         alert("it's a anonym");
+//     }
+// }else if(role === "" || role ===null){
+//     alert("canceled");
+// }else{
+//     alert("i don't know you");
+// }
+
+// let dompet = null;
+
+// alert (dompet ?? 100);
+
+outer : for (let i = 0; i < 3; i++) {
+
+  for (let j = 0; j < 7; j++) {
+
+    if(j%2 == 1) continue;
+    let input = prompt(`Value at coords (${i},${j})`);
+    if (!input) break outer;
+
+  }
 }
 
-alert(age);
-alert(massage);
+alert('Done!');
